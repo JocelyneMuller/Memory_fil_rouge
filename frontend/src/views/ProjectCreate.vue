86e716c8b7aef0af9c0e398a734ed1c1f7e7bbb6
JocelyneMuller <jocelyne.muller@rocket-school.eu>
2025-12-29 19:35:04 +0100

    Ajout de la vue ProjectCreate avec le composant ProjectForm
    Introduit une nouvelle vue ProjectCreate qui affiche le composant ProjectForm et gère l'événement de création de projet. Il comprend un style de base pour centrer le formulaire.

---
+++ frontend/src/views/ProjectCreate.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="project-create-view">
+    <ProjectForm @projectCreated="handleProjectCreated" />
+  </div>
+</template>
+
+<script>
+import ProjectForm from '../components/projects/ProjectForm.vue'
+
+export default {
+  name: 'ProjectCreate',
+  
+  components: {
+    ProjectForm
+  },
+  
+  methods: {
+    handleProjectCreated(result) {
+      console.log('Projet créé:', result)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.project-create-view {
+  min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 20px;
+}
+</style>
