6586993e28e830b2e10f28b9f82bacfa4c0e42da
JocelyneMuller <jocelyne.muller@rocket-school.eu>
2025-12-29 19:30:09 +0100

    Ajout de la page ProjectsView avec le composant ProjectList
    Le composant ProjectsView Vue affiche le composant ProjectList et ainsi qu'un style de base. Cela permet de configurer la structure de la page des projets dans l'interface utilisateur.

---
+++ frontend/src/views/ProjectsView.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="projects-view">
+    <ProjectList :key="refreshKey" />
+  </div>
+</template>
+
+<script>
+import ProjectList from '../components/projects/ProjectList.vue'
+
+export default {
+  name: 'ProjectsView',
+  
+  components: {
+    ProjectList
+  },
+  
+  data() {
+    return {
+      refreshKey: 0
+    }
+  }
+}
+</script>
+
+<style scoped>
+.projects-view {
+  min-height: 100vh;
+}
+</style>
